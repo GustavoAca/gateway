6fcfed7a7f2154861eb163e0d622c7c3238d6c1e
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-19 12:28:20 -0300

    implementando services, comunicacao entre apis e workflow

---
+++ src/main/java/com/glaiss/gateway/config/route/GatewayRoutesConfig.java
@@ -0,0 +1,21 @@
+package com.glaiss.gateway.config.route;
+
+import com.glaiss.gateway.config.security.AuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/users/**")
+                        .filters(f -> f.filter(new AuthenticationFilter()))
+                        .uri("lb://USERS"))
+                .build();
+    }
+}
