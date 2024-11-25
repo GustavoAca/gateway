0f86184f24dcdfac23bcd9ca434ec159b0ebf1ba
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-25 20:07:49 -0300

    criacao filtro no elasticsearch

--- src/main/java/com/glaiss/gateway/GatewayApplication.java
+++ src/main/java/com/glaiss/gateway/GatewayApplication.java
@@ -1,7 +1,10 @@
 package com.glaiss.gateway;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,8 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 }
