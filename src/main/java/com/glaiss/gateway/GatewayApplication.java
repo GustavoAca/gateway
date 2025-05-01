32017f17c8df204f047ecd87b8d8b44a7c88ea3c
Gustavo Acácio <gustavo.acacio@attus.ai>
2025-04-30 21:45:13 -0300

    fix: Possibilitando exibicao da versao do micro pelo actuator

--- src/main/java/com/glaiss/gateway/GatewayApplication.java
+++ src/main/java/com/glaiss/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.glaiss.gateway;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.glaiss.core", "com.glaiss.gateway"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
